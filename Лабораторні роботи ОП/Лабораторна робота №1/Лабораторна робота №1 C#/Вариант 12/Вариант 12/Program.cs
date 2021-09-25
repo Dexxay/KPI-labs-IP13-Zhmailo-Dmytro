946aa235f7f3152bff51cdbd63666f9b0954718c
Dima <90150551+Dexxay@users.noreply.github.com>
2021-09-25 22:35:49 +0300

    Add files via upload

---
+++ Лабораторні роботи ОП/Лабораторна робота №1/Лабораторна робота №1 C#/Вариант 12/Вариант 12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Вариант_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Варіант 12: Задана довжина окола, знайти площу круга, обмеженого цим околом
+            Console.WriteLine("Введiть довжину околу, будь ласка");
+            Console.Write("Довжина околу = ");
+            string type = Console.ReadLine();
+            double radius = Convert.ToDouble(type) / (Math.PI * 2);
+            double result = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine("Площа круга, обмеженого заданим околом = {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
